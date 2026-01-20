9bb76d100e8fdec1e247377502211ad985f7e229
Daniel Vilà <danivila.dev@gmail.com>
2026-01-20 13:18:31 +0100

    feat: create main class on exercise 2 level 1 to make a use case

--- src/main/java/n1exercise2/Main.java
+++ src/main/java/n1exercise2/Main.java
@@ -2,5 +2,20 @@ package n1exercise2;
 
 public class Main {
 
-    //implementar una prueba
+    public static void main(String[] args) {
+
+        Level LevelUser1 = Level.LOW;
+        Level LevelUser2 = Level.MEDIUM;
+        Level LevelUser3 = Level.HIGH;
+
+        Task task1 = new Task(LevelUser1);
+        Task task2 = new Task(LevelUser2);
+        Task task3 = new Task(LevelUser3);
+
+        task1.recomendedBook();
+        task2.recomendedBook();
+        task3.recomendedBook();
+
+    }
+
 }
