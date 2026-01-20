88db8c05c93a39105224bda96f3e20b266b7ead4
Daniel Vilà <danivila.dev@gmail.com>
2026-01-20 10:15:34 +0100

    feat: compliment exercise1 from level 2

---
+++ src/main/java/n2exercise1/Main.java
@@ -0,0 +1,23 @@
+package n2exercise1;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[]args){
+
+        LocalDate localDateObj = LocalDate.now();
+        LocalTime localTimeObj = LocalTime.now();
+        LocalDateTime localDateTimeObj = LocalDateTime.now();
+
+        System.out.println("Showing date with localDate " + localDateObj);
+        System.out.println("Showing time with localTime " + localTimeObj);
+        System.out.println("Showing date and time with localDateTime " + localDateTimeObj);
+
+
+
+    }
+}
